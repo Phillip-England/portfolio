66141ed910e255d8856a1756ac9089f390e8b1ee
Phillip England <phillip.mark.england@gmail.com>
2025-11-18 06:58:10 -0600

    on start ./data will be created if it doesnt exist

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { primeReadMeCache } from "./src/cache/ReadMeCache";
 import { ArticleCache } from "./src/cache/ArticleCache";
 import { Database } from "bun:sqlite";
 import { ContactFormTable } from "./src/database/SqLiteDb";
-
+import { mkdir } from "fs/promises";
 
 let cwd = process.cwd()
 console.log(`current director: ${cwd}`)
@@ -14,6 +14,9 @@ export const articleCache = await ArticleCache.new(
   path.join(cwd, "articles"),
 );
 
+await mkdir(path.join(cwd, "data"), {
+  recursive: true,
+})
 export const db = new Database(path.join(cwd, "data", "main.db"));
 export const contactFormTable =  new ContactFormTable(db)
 
